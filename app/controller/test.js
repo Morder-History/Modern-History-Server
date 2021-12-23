1d5349144ccfa5d46373f29888e54e4a70529333
Silence-dream <1428482231@qq.com>
2021-12-23 17:33:28 +0800

    feat(global): 删除 actions 添加 apidoc

--- app/controller/test.js
+++ app/controller/test.js
@@ -3,6 +3,30 @@
 const Controller = require('egg').Controller;
 
 class TestController extends Controller {
+  /**
+   *
+   * @api {method} /path title
+   * @apiName apiName
+   * @apiGroup group
+   * @apiVersion  1.0.0
+   *
+   * @apiParam  {String} paramName description
+   *
+   * @apiSuccess (200) {type} name description
+   *
+   * @apiParamExample  {type} Request-Example:
+   * {
+   *     property : value
+   * }
+   *
+   *
+   * @apiSuccessExample {type} Success-Response:
+   * {
+   *     property : value
+   * }
+   *
+   *
+   */
   async test() {
     const { ctx } = this;
     const a = await ctx.service.test.sTest();
