a8608c87af36af66115da7db036ed7e02b07567f
silence <1428482231@qq.com>
2022-03-09 20:09:43 +0800

    docs: 修改服务器ip

--- app/docCommon.js
+++ app/docCommon.js
@@ -7,53 +7,53 @@
 
 
 /** 获取首页文章 /api/v1/home 主页
-  *
-  * @api {GET} /api/v1/home 主页
-  * @apiName 主页
-  * @apiGroup Home
-  * @apiVersion  1.0.0
-  * @apiSampleRequest http://47.108.197.220:3010/api/v1/home
-  *
-  */
+ *
+ * @api {GET} /api/v1/home 主页
+ * @apiName 主页
+ * @apiGroup Home
+ * @apiVersion  1.0.0
+ * @apiSampleRequest http://120.27.136.149:3010/api/v1/home
+ *
+ */
 
 
 /** 获取首页二屏 /api/v1/homeTimeLine 主页二屏
-  *
-  * @api {GET} /api/v1/homeTimeLine 主页二屏
-  * @apiName 主页二屏
-  * @apiGroup Home
-  * @apiVersion  1.0.0
-  * @apiSampleRequest http://47.108.197.220:3010/api/v1/homeTimeLine
-  *
-  */
+ *
+ * @api {GET} /api/v1/homeTimeLine 主页二屏
+ * @apiName 主页二屏
+ * @apiGroup Home
+ * @apiVersion  1.0.0
+ * @apiSampleRequest http://120.27.136.149:3010/api/v1/homeTimeLine
+ *
+ */
 
 /** 获取战争信息 /api/v1/warInfo 战争信息
-  *
-  * @api {GET} /api/v1/warInfo 战争信息
-  * @apiName 战争信息
-  * @apiGroup war
-  * @apiVersion  1.0.0
-  * @apiSampleRequest http://47.108.197.220:3010/api/v1/warInfo
-  *
-  */
+ *
+ * @api {GET} /api/v1/warInfo 战争信息
+ * @apiName 战争信息
+ * @apiGroup war
+ * @apiVersion  1.0.0
+ * @apiSampleRequest http://120.27.136.149:3010/api/v1/warInfo
+ *
+ */
 
 /** 获取战争地图标点信息 /api/v1/warTime 战争地图标点信息
-  *
-  * @api {GET} /api/v1/warTime 战争地图标点信息
-  * @apiName 战争地图标点信息
-  * @apiGroup war
-  * @apiVersion  1.0.0
-  * @apiParam  {number} year=1937 年份 1937
-  * @apiSampleRequest http://47.108.197.220:3010/api/v1/warTime
-  *
-  */
+ *
+ * @api {GET} /api/v1/warTime 战争地图标点信息
+ * @apiName 战争地图标点信息
+ * @apiGroup war
+ * @apiVersion  1.0.0
+ * @apiParam  {number} year=1937 年份 1937
+ * @apiSampleRequest http://120.27.136.149:3010/api/v1/warTime
+ *
+ */
 
 /** 获取 918 视频 /api/v1/video918 获取 918 视频
-  *
-  * @api {GET} /api/v1/video918 获取 918 视频
-  * @apiName 获取 918 视频
-  * @apiGroup video
-  * @apiVersion  1.0.0
-  * @apiSampleRequest http://47.108.197.220:3010/api/v1/video918
-  *
-  */
+ *
+ * @api {GET} /api/v1/video918 获取 918 视频
+ * @apiName 获取 918 视频
+ * @apiGroup video
+ * @apiVersion  1.0.0
+ * @apiSampleRequest http://120.27.136.149:3010/api/v1/video918
+ *
+ */
